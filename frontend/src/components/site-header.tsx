d68174f183dde57498e490eab7a712327e6e2cc9
Sapray Minase <sapray@sapray.net>
2026-07-06 16:48:19 +0700

    feat(frontend): update site header layout and styling

--- frontend/src/components/site-header.tsx
+++ frontend/src/components/site-header.tsx
@@ -58,10 +58,10 @@ export function SiteHeader() {
       : "text-amber-500 dark:text-amber-400"
 
   return (
-    <header className="flex h-14 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+    <header className="flex h-[48px] shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+      <div className="flex w-full items-center justify-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
+        <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4 self-center!" />
         <h1 className="text-base font-medium">{title}</h1>
 
         <div className="ml-auto flex items-center gap-2">
